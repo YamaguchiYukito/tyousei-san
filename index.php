bd30bc2b4795b2ec65bd039f10deb02294adaf97
YamaguchiYukito <yamaguchiyuk@centos7.testdom.local>
2020-06-15 18:04:00 +0900

    add php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>
+		Tyousei-san
+		</title>
+	</head>
+	<body>
+	  <p>This is Tyousei-san's page.</p>
+	  <p>Today is <?php echo date("Y/m/d"); ?>.</p>
+	</body>
+</html>
